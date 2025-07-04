b1968a2ddde18556b1ba17cf7daeaac256db0afa
fixorium <fixercoin.token@gmail.com>
2025-07-05 00:23:07 +0500

    Update transaction.py

--- fixorium_wallet/transaction.py
+++ fixorium_wallet/transaction.py
@@ -1,1 +1,8 @@
+ class Transaction:
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
 
+    def __str__(self):
+        return f'Transaction from {self.sender} to {self.recipient} for {self.amount}'
