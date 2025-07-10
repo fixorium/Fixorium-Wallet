22e51c8d9840ffe34d458a4b6636e80875e7bcc5
fixorium <fixercoin.token@gmail.com>
2025-07-11 00:22:35 +0500

    Delete fixorium_wallet directory

--- fixorium_wallet/transaction.py
+++
@@ -1,8 +0,0 @@
- class Transaction:
-    def __init__(self, sender, recipient, amount):
-        self.sender = sender
-        self.recipient = recipient
-        self.amount = amount
-
-    def __str__(self):
-        return f'Transaction from {self.sender} to {self.recipient} for {self.amount}'
