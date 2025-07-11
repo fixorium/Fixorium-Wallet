8a640f2d74d685bc57a55c399f1d563f2c48b34f
fixorium <fixercoin.token@gmail.com>
2025-07-11 20:48:14 +0500

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,1 +1,18 @@
+ import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Wallet from '../components/Wallet';
+import GoogleLogin from '../components/GoogleLogin';
 
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <GoogleLogin />
+      <Wallet />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
