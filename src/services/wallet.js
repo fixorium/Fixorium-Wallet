ab592d90c6925c2b822c6091d492fdb76f67c530
fixorium <fixercoin.token@gmail.com>
2025-07-11 20:58:29 +0500

    Update and rename ... to wallet.js

---
+++ src/services/wallet.js
@@ -0,0 +1,10 @@
+ import { Connection, PublicKey } from '@solana/web3.js';
+
+const connection = new Connection('https://api.devnet.solana.com');
+
+const getBalance = async () => {
+  // Implement get balance logic here
+  return 0;
+};
+
+export { getBalance };
