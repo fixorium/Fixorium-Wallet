e3f292ffa31b4d37cc13b6ce9e5b031d2edde1fd
fixorium <fixercoin.token@gmail.com>
2025-07-13 15:17:09 +0500

    Delete src directory

--- src/utils/solana.js
+++
@@ -1,5 +0,0 @@
- import { Connection, PublicKey } from '@solana/web3.js';
-
-const connection = new Connection('https://api.devnet.solana.com');
-
-export { connection };
--- src/utils/web3.js
+++
@@ -1,5 +0,0 @@
-import Web3 from 'web3';
-
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_PROJECT_ID'));
-
-export default web3;
